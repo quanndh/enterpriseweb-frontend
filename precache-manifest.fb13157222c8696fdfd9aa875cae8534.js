a1924f349eb3ac45117f5ac6262e537b8574d59a
quannguyennn <wantmorexx9x@gmail.com>
2020-04-24 09:25:05 +0700

    Updates

---
+++ precache-manifest.fb13157222c8696fdfd9aa875cae8534.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6667fb0b1eab163fbb00cbda705d228f",
+    "url": "/enterpriseweb-frontend/index.html"
+  },
+  {
+    "revision": "cd82afb013dc5dbaaa2a",
+    "url": "/enterpriseweb-frontend/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "91263c28edb196c4439d",
+    "url": "/enterpriseweb-frontend/static/css/main.e69e9870.chunk.css"
+  },
+  {
+    "revision": "cd82afb013dc5dbaaa2a",
+    "url": "/enterpriseweb-frontend/static/js/2.57f680ea.chunk.js"
+  },
+  {
+    "revision": "6701128802f7454a9e906f15715310d0",
+    "url": "/enterpriseweb-frontend/static/js/2.57f680ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91263c28edb196c4439d",
+    "url": "/enterpriseweb-frontend/static/js/main.aef8b7d1.chunk.js"
+  },
+  {
+    "revision": "0f8c2f21dd351e90ab78",
+    "url": "/enterpriseweb-frontend/static/js/runtime-main.b876bdaa.js"
+  },
+  {
+    "revision": "9471c4fe1d112c678685cb1407da763e",
+    "url": "/enterpriseweb-frontend/static/media/afternoon.9471c4fe.jpg"
+  },
+  {
+    "revision": "f620e5fdb9a2e0b60d45c119586761e4",
+    "url": "/enterpriseweb-frontend/static/media/avatar.f620e5fd.svg"
+  },
+  {
+    "revision": "374a04af25e7376c1926c16afa34974f",
+    "url": "/enterpriseweb-frontend/static/media/bg.374a04af.svg"
+  },
+  {
+    "revision": "6985874d2b78b1d434db209097a01f41",
+    "url": "/enterpriseweb-frontend/static/media/evening.6985874d.jpg"
+  },
+  {
+    "revision": "e0c69bd2c0d2ac31838c2963ef0c7a37",
+    "url": "/enterpriseweb-frontend/static/media/fbsound.e0c69bd2.mp3"
+  },
+  {
+    "revision": "a6aa08a4a3094d04faffda9f20aa5018",
+    "url": "/enterpriseweb-frontend/static/media/morning.a6aa08a4.jpg"
+  },
+  {
+    "revision": "6d252a00c562aee3ae3bab8ed4658f52",
+    "url": "/enterpriseweb-frontend/static/media/wave.6d252a00.png"
+  }
+]);
