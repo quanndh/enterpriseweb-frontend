dfbdebfc6feb3863b2f5a74a01d9cb1d01e2bf8d
quannguyennn <wantmorexx9x@gmail.com>
2020-04-24 09:48:29 +0700

    .

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    HOST: process.env.NODE_ENV === "production" ? "https://xtutoring.herokuapp.com" : 'http://localhost:1337'
+    HOST: process.env.NODE_ENV === "production" ? "https://xtutoring.herokuapp.com" : 'https://xtutoring.herokuapp.com'
 }
